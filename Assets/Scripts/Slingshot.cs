4d144da332626da019eaf7b26e6be4ac80bfad66
Joe watson <41978879+Jaydanasara@users.noreply.github.com>
2020-02-10 23:44:50 -0500

    instantiated projectile

---
+++ Assets/Scripts/Slingshot.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slingshot : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [Header("Set in Inspector")]
+
+    public GameObject           prefabProjectile;
+ [Header("Set Dynamically")]
+    public GameObject           launchPoint;
+    public Vector3              launchPos;
+    public GameObject           projectile;
+    public bool                 aimingMode;
+
+ void Awake() {
+        Transform launchPointTrans = transform.Find("LaunchPoint");
+        launchPoint = launchPointTrans.gameObject;
+        launchPoint.SetActive(false);
+        launchPos = launchPointTrans.position;    }
+
+    void OnMouseEnter() {
+        launchPoint.SetActive(true);
+    }
+
+
+ void OnMouseExit() {
+      launchPoint.SetActive(false);
+    }
+
+ void OnMouseDown() {
+        aimingMode=true;
+        projectile = Instantiate(prefabProjectile) as GameObject;
+        projectile.transform.position = launchPos;
+        projectile.GetComponent<Rigidbody>().isKinematic= true;
+    }
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
