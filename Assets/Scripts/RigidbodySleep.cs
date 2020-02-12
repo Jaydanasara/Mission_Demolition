7176054157a2216fd664eaa1640171668d33c69b
Joe watson <41978879+Jaydanasara@users.noreply.github.com>
2020-02-11 20:47:31 -0500

    castle1

--- Assets/Scripts/RigidbodySleep.cs
+++ Assets/Scripts/RigidbodySleep.cs
@@ -7,6 +7,9 @@ public class RigidbodySleep : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Rigidbody rb = GetComponent<Rigidbody>();
+
+        if (rb != null) rb.Sleep();
         
     }
 
